67074662e2302c781aacb0df6abc289c8853e0c5
dv1j <dvijpurmanun1@gmail.com>
2026-08-10 12:10:10 +0400

    removed whitespace on line 6

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     lad = -(-number % 10)
-else: 
+else:
     lad = number % 10
 if lad > 5:
     print(f"Last digit of {number} is {lad} and is greater than 5")
