cbaa129e71946a94766ccdc159b615a9ed31dbab
dv1j <dvijpurmanun1@gmail.com>
2026-08-11 13:54:32 +0400

    task 5 (python is fasinating but also so annoying)

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    if i < 10:
-        print(f"0{i}",end="")
-    if i >= 10:
-        print(f"{i}",end="")
-    if i != 99:
-        print(","," ",end="")
+    if i < 99:
+        print(f"{i:02d}", end=", ")
     else:
-        print("\n")
+        print(f"{i:02d}")
