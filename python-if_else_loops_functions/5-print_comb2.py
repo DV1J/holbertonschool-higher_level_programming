dd4193c30702ddeed0ccf03b118d3b29dc590742
dv1j <dvijpurmanun1@gmail.com>
2026-08-11 10:33:25 +0400

    test task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+for i in range(100):
+    if i < 10:
+        print(f"0{i}",end="")
+    if i >= 10:
+        print(f"{i}",end="")
+    if i != 99:
+        print(","," ",end="")
+    else:
+        print("\n")
