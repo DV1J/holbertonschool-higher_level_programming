e9b711826b452c0fb29ca4832b1d065a10895cb1
dv1j <dvijpurmanun1@gmail.com>
2026-08-17 10:57:01 +0400

    task 9 test

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def print_last_digit(number): 
+def print_last_digit(number):
+    if number < 0:
+        lad = -(-number % 10)
     lad = number % 10
-    return lad
+    print(lad, end='')
+    return(lad)
