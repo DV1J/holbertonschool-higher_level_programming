fa813c1462c192062c22084362be0977975df19b
dv1j <dvijpurmanun1@gmail.com>
2026-08-17 11:50:24 +0400

    change -number

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        number = -(-number % 10)
+        number = -number
     lad = number % 10
     print(lad, end='')
     return (lad)
