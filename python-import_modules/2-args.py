340956ea56192f66fe4698a02615a0c4baa199ae
dv1j <dvijpurmanun1@gmail.com>
2026-08-24 14:33:41 +0400

    add '.' to line 7 output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     i = 1
     num = len(argv[1:])
     if num == 0:
-        print(num, 'arguments')
+        print(num, 'arguments.')
     elif num == 1:
         print(num, 'argument:')
     elif num >= 2:
