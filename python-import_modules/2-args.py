15113573bb08ff66ce92e6f6a15d9002982b708e
dv1j <dvijpurmanun1@gmail.com>
2026-08-24 14:30:42 +0400

    finally doing with task 2 (hopefully)

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    num = len(argv)
-    if num == 1:
-        print('0', 'arguments')
-    elif num == 2:
-        print('1', 'argument')
-    elif num > 2:
+    i = 1
+    num = len(argv[1:])
+    if num == 0:
         print(num, 'arguments')
+    elif num == 1:
+        print(num, 'argument:')
+    elif num > 2:
+        print(num, 'arguments:')
     for j in argv[1:]:
-        print("{}: {}".format(num, j)) 
+        print("{}: {}".format(i, j))
+        i += 1
