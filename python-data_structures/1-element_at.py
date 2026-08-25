3533c8d43e56a7a47495f1240b92dd32abf16cd9
dv1j <dvijpurmanun1@gmail.com>
2026-08-25 13:50:55 +0400

    task 1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx > len(my_list):
+        return (None)
+    if idx == "-":
+        return (None)
+    else:
+        return (my_list[idx])
