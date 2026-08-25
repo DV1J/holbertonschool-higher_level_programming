1377873350e6766c26e2979673581be41adcf8d1
dv1j <dvijpurmanun1@gmail.com>
2026-08-25 10:19:58 +0400

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    sum = 0
+    if len(argv[1:]) == 0:
+        print('0')
+    for i in (argv[1:]):
+        sum = sum + int(i)
+    print(sum)
