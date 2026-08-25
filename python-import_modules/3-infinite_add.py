761407085a836bd89555f26402e0efe758849ac7
dv1j <dvijpurmanun1@gmail.com>
2026-08-25 10:21:44 +0400

    fixed output "0 , 0"

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,8 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     sum = 0
-    if len(argv[1:]) == 0:
-        print('0')
     for i in (argv[1:]):
         sum = sum + int(i)
     print(sum)
