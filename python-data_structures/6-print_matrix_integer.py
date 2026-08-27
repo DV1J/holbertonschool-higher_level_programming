2e33dea4c7469bbf9088b38bd3b2a4f30131683e
dv1j <dvijpurmanun1@gmail.com>
2026-08-27 13:35:28 +0400

    trying another fix (added another if statment at line 8)

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            if j != '':
-                print("{:d}".format(j),end=' ')
-            if j % 3 == 0:
-                print('')
-            
+            if j % 3 != 0:
+                print("{:d}".format(j), end=' ')
+            elif j % 3 == 0:
+                print("{:d}".format(j), '')
