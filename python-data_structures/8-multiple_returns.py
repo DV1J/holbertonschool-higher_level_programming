175e4f6d8284e266d51aaa9eb744654faa9855ed
dv1j <dvijpurmanun1@gmail.com>
2026-08-27 14:21:05 +0400

    task 8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if sentence == '':
+        first = 'None'
+        return (length, first)
+    else:
+        first = sentence[0]
+        return (length, first)
