acef01428408fb34993fb5120c657d304a303f6e
dv1j <dvijpurmanun1@gmail.com>
2026-08-31 10:06:10 +0400

    task 9

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    ta = list(tuple_a)
+    tb = list(tuple_b)
+    for i in range(len (tb)):
+        if ta[i] < 2 or tb[i] < 2:
+             ta[i] = 0
+             tb[i] = 0
+        add = (ta[i] + tb[i])
+        print(add, end=' ')
