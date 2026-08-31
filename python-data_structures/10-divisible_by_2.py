b37ba4cb69f9410b7db407e3e91196227f4866a5
dv1j <dvijpurmanun1@gmail.com>
2026-08-31 14:18:27 +0400

    fix get error

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     new_list = my_list[:]
     for i in range(len(my_list)):
-        if i % 2 == 0:
-            new_list[i] = True
+        if new_list[i] % 2 == 0:
+            new_list = True
         else:
             new_list[i] = False
     return (new_list)
