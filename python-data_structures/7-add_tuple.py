94adff432709ea57b39f6184da783810003ab9dd
dv1j <dvijpurmanun1@gmail.com>
2026-09-01 10:29:20 +0400

    save just in case

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,9 +2,11 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     ta = list(tuple_a)
     tb = list(tuple_b)
-    for i in range(len (tb)):
-        if ta[i] < 2 or tb[i] < 2:
-             ta[i] = 0
-             tb[i] = 0
-        add = (ta[i] + tb[i])
-        print(add, end=' ')
+    for i in range(len(tb)):
+        for j in range(len(tb)):
+            if ta > tb:
+                tb[i] = 0
+                tb[j] = 0
+            add = (ta[i] + tb[i])
+            add_2 = (ta[1] + tb[j])
+        return(add, add_2)
