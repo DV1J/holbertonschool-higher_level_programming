c36ea21345f4ae900e4b69036efe2e2c3f26e488
dv1j <dvijpurmanun1@gmail.com>
2026-09-01 11:14:02 +0400

    fixed pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -8,10 +8,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             ta = ta[0], 0
     if len(tb) < 2:
-            if len(tb) == 0:
-                tb = 0, 0
-            else:
-                tb = tb[0], 0
+        if len(tb) == 0:
+            tb = 0, 0
+        else:
+            tb = tb[0], 0
     add = (ta[0] + tb[0])
     add_2 = (ta[1] + tb[1])
-    return(add, add_2)
+    return (add, add_2)
