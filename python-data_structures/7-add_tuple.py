53b4ecef1099e9093fe7632adade001f1b3ca37c
dv1j <dvijpurmanun1@gmail.com>
2026-09-01 10:51:39 +0400

    test task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,11 +2,16 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     ta = list(tuple_a)
     tb = list(tuple_b)
-    for i in range(len(tb)):
-        for j in range(len(tb)):
-            if ta > tb:
-                tb[i] = 0
-                tb[j] = 0
-            add = (ta[i] + tb[i])
-            add_2 = (ta[1] + tb[j])
-        return(add, add_2)
+    if len(tb) < 2:
+        if len(ta) == 0:
+            ta = 0, 0
+        else:
+            ta = ta[0], 0
+    if len(tb) < 2:
+        if len(tb) == 0:
+            tb = 0, 0
+        else:
+            tb = tb[0], 0
+    add = (ta[0] + tb[0])
+    add_2 = (ta[1] + tb[1])
+    return (add, add_2)
