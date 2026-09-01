f89b0f1a0403e5482926077d3bd302d1f17e7b2a
dv1j <dvijpurmanun1@gmail.com>
2026-09-01 11:12:52 +0400

    changed error line 5 if len(tb) <2 to if len(ta) < 2

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    ta = list(tuple_a)
-    tb = list(tuple_b)
-    if len(tb) < 2:
+    ta = tuple_a
+    tb = tuple_b
+    if len(ta) < 2:
         if len(ta) == 0:
             ta = 0, 0
         else:
             ta = ta[0], 0
     if len(tb) < 2:
-        if len(tb) == 0:
-            tb = 0, 0
-        else:
-            tb = tb[0], 0
-    return(ta[0] + tb[0], ta[1] + tb[1])
+            if len(tb) == 0:
+                tb = 0, 0
+            else:
+                tb = tb[0], 0
+    add = (ta[0] + tb[0])
+    add_2 = (ta[1] + tb[1])
+    return(add, add_2)
