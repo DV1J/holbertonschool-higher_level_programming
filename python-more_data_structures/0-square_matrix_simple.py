33e6420b794aa32c6a69e7316acafa587236c807
dv1j <dvijpurmanun1@gmail.com>
2026-09-03 14:24:41 +0400

    task 0 removed useless loops and map statments

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,15 +4,9 @@ def square(a):
 
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix[:]
+    new_matrix = []
     for num in range(len(matrix)):
         for num1 in range(len(matrix[num])):
-            num1 += 1
-            for num2 in range(len(matrix[num1])):
-                num2 += 2
-                sq1 = map(square, (new_matrix[num]))
-                sq2 = map(square, (new_matrix[num1]))
-                sq3 = map(square, (new_matrix[num2]))
-                sq = list(sq1), list(sq2), list(sq3)
-                return (list(sq))
-
+            sq = map(square, (matrix[num1]))
+            new_matrix.append(list(sq))
+        return (new_matrix)
