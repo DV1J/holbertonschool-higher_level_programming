74a8dab8c4fa2f997a5405cb4802f4e065436e3c
dv1j <dvijpurmanun1@gmail.com>
2026-09-14 11:26:30 +0400

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    final_list = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            final_list += 1
+        except IndexError:
+            continue
+    print(' ')
+    return (final_list)
