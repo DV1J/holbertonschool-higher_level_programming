f5fae8f67d1fa72d5c2d01f94c57be07892653b2
dv1j <dvijpurmanun1@gmail.com>
2026-09-21 13:51:10 +0400

    fix pycodestyle and added #!/usr/bin/python3

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,18 @@
+#!/usr/bin/python3
 """
 A function adds 2 values
 returns the added value in type integer
 
 """
+
+
 def add_integer(a, b=98):
-        """
-        checking if a and b are of int and float type if not raise TypeError with custom message
-        """
-        if type(a) != int and type(a) != float:
-                raise TypeError('a must be an integer')
-        if type(b) != int and type(b) != float:
-                raise TypeError('b must be an integer')
-        add = a + b
-        return (int(add))
+    """checking if a and b are of int and float type
+    if not raise TypeError with custom message
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
+    add = a + b
+    return (int(add))
