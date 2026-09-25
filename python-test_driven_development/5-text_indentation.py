fcf05cb74d2c3896b3f73a733a383964fd0fc369
dv1j <dvijpurmanun1@gmail.com>
2026-09-25 09:41:07 +0400

    changed code back to the way it was and also changed "!=" to "is not" in file 5-text_indentation.py line 9

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,11 +6,11 @@
 def text_indentation(text):
     """Checking is text is a string if not Raise TypeError
     """
-    if type(text) != str:
+    if type(text) is not str:
         raise TypeError('text must be a string')
     for i in text:
         if i != '.' and i != '?' and i != ':':
             print(i, end='')
         else:
             print(i)
-            
+            print()
