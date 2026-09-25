975e3d4776ac6756c1e2ed861ed99b57266b5b07
dv1j <dvijpurmanun1@gmail.com>
2026-09-25 09:33:34 +0400

    task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A function that indents texts after (., ?, :)
+"""
+
+
+def text_indentation(text):
+    """Checking is text is a string if not Raise TypeError
+    """
+    if type(text) != str:
+        raise TypeError('text must be a string')
+    for i in text:
+        if i != '.' and i != '?' and i != ':':
+            print(i, end='')
+        else:
+            print(i)
+            print()
